4d68a04baa2bb34ae46946d436fa6e143fdf6c53
Sergei <50073732+systanon@users.noreply.github.com>
2019-11-15 21:35:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest
+request.onreadystatechange = function (event) {
+	this.readyState === 4 ? this.status === 200 ? 
+		console.log(this.responseText) :
+			console.warn('request error') :
+				null
+}
+request.open('GET','./user.json')
+request.send()
