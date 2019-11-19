7f37ebe1e885912d9a50e6b7d35cba15183cd7ca
Sergei <50073732+systanon@users.noreply.github.com>
2019-11-19 19:02:25 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,20 @@
-var catsGithub
-var request = Object.assign(
-  new XMLHttpRequest, {
-    onreadystatechange :function (event) {
-      this.readyState === 4 ? this.status === 200 ?
-       console.log(this.responseText) :
-          console.warn('error') : null
+
+
+function getData (url,callback) {
+  var request = Object.assign(
+    new XMLHttpRequest, {
+      onreadystatechange :function (event) {
+        this.readyState === 4 ? this.status === 200 ?
+          callback(this.responseText) :
+             console.warn('error') : null
+      }
     }
-  }
-)
-request.open("GET","./user2.json")
-request.send()
+  )
+  request.open("GET",url)
+  request.send()
+}
+var func = function (response) {
+  console.log(response)
+}
+
+getData("./user2.json",func)
